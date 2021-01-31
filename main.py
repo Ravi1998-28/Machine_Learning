ec7f52fd841a6ff2bfeb2446fc849bd8c394df6d
Ravi Chauhan <ravi.chauhan2806@gmail.com>
2021-01-31 22:55:11 +0530

    committed

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ diabetes_y_predicted = model.predict(diabetes_x_test)
 print('mean square error in system:', mean_squared_error(diabetes_y_test, diabetes_y_predicted))
 print('weights: ', model.coef_)
 print('inteceptes: ', model.intercept_)
+print('inteceptes: ', model.intercept_)
 
 # plt.scatter(diabetes_x_test, diabetes_y_predicted)
 # plt.plot(diabetes_x_test, diabetes_y_predicted)
