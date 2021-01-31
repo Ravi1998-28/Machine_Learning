860a5a08fcbe13cf74f0657e304c10786b6b745c
Ravi Chauhan <ravi.chauhan2806@gmail.com>
2021-01-31 22:58:31 +0530

    committed

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from sklearn.metrics import mean_squared_error
 diabetes = datasets.load_diabetes()
 
 diabetes_x = diabetes.data
-# this is new comment
 diabetes_x_train = diabetes_x[:-30]
 diabetes_x_test = diabetes_x[-30:]
 
@@ -22,9 +21,10 @@ diabetes_y_predicted = model.predict(diabetes_x_test)
 print('mean square error in system:', mean_squared_error(diabetes_y_test, diabetes_y_predicted))
 print('weights: ', model.coef_)
 print('inteceptes: ', model.intercept_)
-print('inteceptes: ', model.intercept_)
+
 
 # plt.scatter(diabetes_x_test, diabetes_y_predicted)
 # plt.plot(diabetes_x_test, diabetes_y_predicted)
 
 # plt.show()
+# need more dedication
